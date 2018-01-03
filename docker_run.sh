ce7d349b18a5a809780d1f3b6de15f21a54330c0
xasdx <xasdx@tuta.io>
2018-01-03 17:39:55 +0100

    Add scripts for building and running images

---
+++ docker_run.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+docker run -p 3210:8080 -d asdx/sandbox-docker
--- index.js
+++ index.js
@@ -3,7 +3,7 @@ let app = express()
 
 app.get("/", (req, res) => res.send("hello"))
 
-let port = process.env.PORT
+let port = process.env.PORT || 8080
 
-app.listen(8080, "0.0.0.0")
+app.listen(port, "0.0.0.0")
 console.log(`server up on port ${port}`)
