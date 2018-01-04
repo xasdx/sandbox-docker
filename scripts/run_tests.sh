ceab197a12f951e6564e125985938b9f31073f18
xasdx <xasdx@tuta.io>
2018-01-04 19:26:10 +0100

    Add cleanup to the beginning of run_tests.sh

--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
+docker-compose -p ci kill
+docker-compose -p ci rm -f
 docker-compose -f docker-compose.test.yml -p ci build
 docker-compose -f docker-compose.test.yml -p ci up -d
-EXIT_CODE = `docker wait ci_sut_1`
+EXIT_CODE=`docker wait ci_sut_1`
 docker logs -f ci_sut_1
 exit $EXIT_CODE
