39c2607cdbfe8e9c099147fd180c20209b762874
_asdx_ <xasdx@tuta.io>
2018-01-05 16:52:41 +0000

    Fix failing test

--- test.js
+++ test.js
@@ -1,6 +1,6 @@
 let test = require("ava")
 
-let getResult = () => new Promise((ok, nope) => setTimeout(() => ok(421), 500))
+let getResult = () => new Promise((ok, nope) => setTimeout(() => ok(420), 500))
 
 test("tests stuff", async t => {
   t.is(await getResult(), 420)
